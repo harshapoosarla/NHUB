c32be838021ab3ffdfb2e2e6fe5416b57b14df87
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-09-29 11:52:08 +0530

    c1

---
+++ Project_28Sep2018/Project_28Sep2018/SAServiceLines.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_28Sep2018
+{
+    public partial class ServiceLines : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void addserviceline_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("SAServiceLinesAdd.aspx");
+        }
+    }
+}
