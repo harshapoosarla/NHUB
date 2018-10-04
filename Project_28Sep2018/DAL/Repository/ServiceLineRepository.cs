718b4fd3416ed07a85d934d70dcd54522145b2fd
harshapoosarla <43164668+harshapoosarla@users.noreply.github.com>
2018-10-04 18:53:19 +0530

    commit

--- Project_28Sep2018/DAL/Repository/ServiceLineRepository.cs
+++ Project_28Sep2018/DAL/Repository/ServiceLineRepository.cs
@@ -36,10 +36,8 @@ namespace DAL.Repository
             }
             return servicelinelist;
         }
-
         public DataTable getServLineDetails(string pServLineId)
         {
-
             SqlDataAdapter adapter;
             using (SqlConnection connection = new SqlConnection())
             {
@@ -49,15 +47,9 @@ namespace DAL.Repository
                 adapter = new SqlDataAdapter(sql, connection);
                 //SqlCommand sqlCommand = new SqlCommand(sql, connection);
                 adapter.Fill(ServLineManTab);
-
             }
             return ServLineManTab;
         }
-
-
-
-
-
         public void InsertServiceLine(string pServLineName, string pSLMids)
         {
             using (SqlConnection connection = new SqlConnection())
@@ -66,7 +58,6 @@ namespace DAL.Repository
                 connection.Open();
                 SqlCommand sqlCommand = new SqlCommand("Proc_InsertServiceLine", connection);
                 sqlCommand.CommandType = CommandType.StoredProcedure;
-
                 //sqlCommand.Parameters.Add("@pServLineName", SqlDbType.NVarChar, 50).Value = pServLineName;
                 int ServLineId = 0;//=Convert.ToInt32(sqlCommand.Parameters.Add("@pServLineId", SqlDbType.NVarChar, 50).Value);
                 SqlParameter parServLineName = new SqlParameter()
@@ -80,7 +71,6 @@ namespace DAL.Repository
                 SqlParameter parServiceLineId = new SqlParameter()
                 {
                     ParameterName = "@pServLineId",
-
                     SqlDbType = SqlDbType.Int,
                     Value = ServLineId,
                     Direction = ParameterDirection.Output
@@ -91,16 +81,11 @@ namespace DAL.Repository
                 string[] Slm = pSLMids.Split(',');
                 foreach (string slms in Slm)
                 {
-
-
                     SqlCommand sqlCommand1 = new SqlCommand("Proc_InsertServLineManagers", connection);
                     sqlCommand1.CommandType = CommandType.StoredProcedure;
                     //sqlCommand1.Parameters.Add("@pId", SqlDbType.Int).Value =pId;
                     sqlCommand1.Parameters.Add("@pServiceLineId", SqlDbType.Int).Value = ServLineId;
                     sqlCommand1.Parameters.Add("@pUserId", SqlDbType.NVarChar).Value = slms;
-
-
-
                     sqlCommand1.ExecuteNonQuery();
                 }
                 connection.Close();
@@ -117,7 +102,6 @@ namespace DAL.Repository
             sqlCommand.ExecuteNonQuery();
             connection.Close();
         }
-
         public void EditServiceLine(int pServLineId, string pSLMids)//copy
         {
             using (SqlConnection connection = new SqlConnection())
@@ -126,14 +110,11 @@ namespace DAL.Repository
                 connection.Open();
                 SqlCommand sqlCommand = new SqlCommand("Proc_EditServLineManagers", connection);
                 sqlCommand.CommandType = CommandType.StoredProcedure;
-
                 //sqlCommand.Parameters.Add("@pServLineName", SqlDbType.NVarChar, 50).Value = pServLineName;
                 //int ServLineId = 0;//=Convert.ToInt32(sqlCommand.Parameters.Add("@pServLineId", SqlDbType.NVarChar, 50).Value);
-
                 SqlParameter parServiceLineId = new SqlParameter()
                 {
                     ParameterName = "@pServiceLineId",
-
                     SqlDbType = SqlDbType.Int,
                     Value = pServLineId,
                     Direction = ParameterDirection.Input
@@ -144,16 +125,11 @@ namespace DAL.Repository
                 var Slm1 = Slm.Distinct();
                 foreach (string slms in Slm1)
                 {
-
-
                     SqlCommand sqlCommand1 = new SqlCommand("Proc_InsertServLineManagers", connection);
                     sqlCommand1.CommandType = CommandType.StoredProcedure;
                     //sqlCommand1.Parameters.Add("@pId", SqlDbType.Int).Value =pId;
                     sqlCommand1.Parameters.Add("@pServiceLineId", SqlDbType.Int).Value = pServLineId;
                     sqlCommand1.Parameters.Add("@pUserId", SqlDbType.NVarChar).Value = slms;
-
-
-
                     sqlCommand1.ExecuteNonQuery();
                 }
                 connection.Close();
@@ -169,21 +145,15 @@ namespace DAL.Repository
                 var Slm1 = Slm.Distinct();
                 foreach (string slms in Slm1)
                 {
-
-
                     SqlCommand sqlCommand1 = new SqlCommand("Proc_InsertServLineManagers", connection);
                     sqlCommand1.CommandType = CommandType.StoredProcedure;
                     //sqlCommand1.Parameters.Add("@pId", SqlDbType.Int).Value =pId;
                     sqlCommand1.Parameters.Add("@pServiceLineId", SqlDbType.Int).Value = pServLineId;
                     sqlCommand1.Parameters.Add("@pUserId", SqlDbType.NVarChar).Value = slms;
-
-
-
                     sqlCommand1.ExecuteNonQuery();
                 }
                 connection.Close();
             }
-
         }
         public void EditServiceLineOM(int pServLineId, string pOMids)
         {
@@ -193,47 +163,31 @@ namespace DAL.Repository
                 connection.Open();
                 SqlCommand sqlCommand = new SqlCommand("Proc_EditServLineOpManagers", connection);
                 sqlCommand.CommandType = CommandType.StoredProcedure;
-
                 //sqlCommand.Parameters.Add("@pServLineName", SqlDbType.NVarChar, 50).Value = pServLineName;
                 //int ServLineId = 0;//=Convert.ToInt32(sqlCommand.Parameters.Add("@pServLineId", SqlDbType.NVarChar, 50).Value);
-
                 SqlParameter parServiceLineId = new SqlParameter()
                 {
                     ParameterName = "@pServiceLineId",
-
                     SqlDbType = SqlDbType.Int,
                     Value = pServLineId,
                     Direction = ParameterDirection.Input
                 };
                 sqlCommand.Parameters.Add(parServiceLineId);
                 sqlCommand.ExecuteNonQuery();
-
                 string[] Slm = pOMids.Split(',');
                 var Slm1 = Slm.Distinct();
                 foreach (string slms in Slm1)
                 {
-
-
                     SqlCommand sqlCommand1 = new SqlCommand("Proc_InsertServLineManagers", connection);
                     sqlCommand1.CommandType = CommandType.StoredProcedure;
                     //sqlCommand1.Parameters.Add("@pId", SqlDbType.Int).Value =pId;
                     sqlCommand1.Parameters.Add("@pServiceLineId", SqlDbType.Int).Value = pServLineId;
                     sqlCommand1.Parameters.Add("@pUserId", SqlDbType.NVarChar).Value = slms;
-
-
-
                     sqlCommand1.ExecuteNonQuery();
                 }
                 connection.Close();
             }
-
-
-
-
         }
-
-
-
     }
     public class SelectAllUsers
     {
@@ -261,7 +215,6 @@ namespace DAL.Repository
                 }
             }
             return servicelinelist;
-
         }
     }
     public class SLMServiceLine
@@ -271,7 +224,6 @@ namespace DAL.Repository
         public DataTable ServLineOMTab = new DataTable();
         public List<ServiceLine> getSLDetails(string pUserId)
         {
-
             using (SqlConnection connection = new SqlConnection())
             {
                 connection.ConnectionString = @"Data Source=ACULAP-119;Initial Catalog=NotificationHub;Integrated Security=True";
@@ -296,7 +248,6 @@ namespace DAL.Repository
         }
         public DataTable getServLineOMDetails(string pServLineId)
         {
-
             SqlDataAdapter adapter;
             using (SqlConnection connection = new SqlConnection())
             {
@@ -306,7 +257,6 @@ namespace DAL.Repository
                 adapter = new SqlDataAdapter(sql, connection);
                 //SqlCommand sqlCommand = new SqlCommand(sql, connection);
                 adapter.Fill(ServLineOMTab);
-
             }
             return ServLineOMTab;
         }
@@ -332,9 +282,7 @@ namespace DAL.Repository
                 }
             }
             return ServLineOMList;
-
         }
-
         public DataTable getAllOMDetailsOfServiceLineManagers(string pServLineId)
         {
              List<Users> ServLineOMListForSLM = new List<Users>();
@@ -345,8 +293,7 @@ namespace DAL.Repository
                 connection.ConnectionString = @"Data Source=ACULAP-119;Initial Catalog=NotificationHub;Integrated Security=True";
                 connection.Open();
                 string sql = "select distinct a.UserName,a.Id from AspNetUsers a,OperationManager s where ServiceLineId =" + pServLineId;
-
-               // SqlCommand sqlCommand = new SqlCommand(sql, connection);
+                // SqlCommand sqlCommand = new SqlCommand(sql, connection);
                 //  sqlCommand.CommandType = CommandType.StoredProcedure;
                 //sqlCommand.Parameters.Add("@Action", SqlDbType.VarChar, 250).Value = "SELECT";
                 //using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
@@ -365,9 +312,6 @@ namespace DAL.Repository
                 adapterSLM.Fill(ServLineOMTabForSLM);
             }
             return ServLineOMTabForSLM;
-
         }
-
-
     }
 }
